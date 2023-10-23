73525d8541b6a33d42150fd86c329665a3bb7997
Stephen Nemeth <stephen.j.nemeth@outlook.com>
2023-10-22 20:43:46 -0400

    Added stuff for wokring with login and checking login status

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import NavBar from './components/NavBar';
 import HomePage from './components/HomePage';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
@@ -19,18 +19,25 @@ import ManagePromotions from './components/ManagePromotions';
 const App = () => {
   
   const [search, setSearch] = useState('')
+  const [user, setUser] = useState(null)
   
+  useEffect(() => {
+    if (localStorage.getItem("user") !== null) {
+      setUser(localStorage.getItem("user"))
+    }
+  })
+
   return (
     <div className='App'>
-      <NavBar setSearch={setSearch} />
+      <NavBar setSearch={setSearch} user={user} setUser={setUser}/>
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<HomePage search={search} />} />
+          <Route path='/' element={<HomePage search={search}/>} />
           <Route path='/signup' element={<SignUpPage />} />
           <Route path='/login' element={<LoginPage />} />
           <Route path='/admin' element={<AdminMainPage />} />
           <Route path="/admin/movies" element={<ManageMovies />} />
-          <Route path='/updateprofile' element={<UpdateProfilePage />} />
+          <Route path='/updateprofile' element={<UpdateProfilePage user={user}/>} />
           <Route path='/purchasehistory' element={<PurchaseHistory />} />
           <Route path='/checkout' element={<CheckoutPage />} />
           <Route path='/admin/manageusers' element={<ManageUsersPage />} />
--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -5,18 +5,23 @@ import logo from '../eCinemaBooking.png'
 
 import { Button, FormControl, Navbar, Container, Form, Nav, Image } from 'react-bootstrap'
 import NavbarCollapse from 'react-bootstrap/esm/NavbarCollapse'
+import { useNavigate } from 'react-router-dom'
 
 
 const NavBar = (props) => {
 
     const [query, setQuery] = useState('')
+    const navigate = useNavigate
 
     const onSearch = () => {
         props.setSearch(query)
         setQuery('')
     }
 
-    
+    const logout = () => {
+        localStorage.setItem("user", null)
+        navigate("/")
+    }
 
     return (
         <Navbar bg='primary'  data-bs-theme="dark">
@@ -26,11 +31,21 @@ const NavBar = (props) => {
                 </Navbar.Brand>
                 <NavbarCollapse>
                     <Nav>
-                        <Nav.Link className='navbar-link' href='/login'>Login</Nav.Link>
-                        <Nav.Link className='navbar-link' href='/signup'>Sign Up</Nav.Link>
                         <Nav.Link className='navbar-link' href="/">Movies</Nav.Link>
-                        <Nav.Link className='navbar-link' href="/admin">Admin</Nav.Link>
-                        <Nav.Link className='navbar-link' href="/updateprofile">Edit Profile</Nav.Link>
+                        {!props.user &&
+                            <>
+                                <Nav.Link className='navbar-link' href='/login'>Login</Nav.Link>
+                                <Nav.Link className='navbar-link' href='/signup'>Sign Up</Nav.Link>
+                            </>
+                        }
+                        {props.user && (
+                            <>
+                                <Nav.Link className='navbar-link' href="/updateprofile">Edit Profile</Nav.Link>
+                                <Nav.Link className='navbar-link' onClick={logout}>Logout</Nav.Link>
+                                {props.user.userTypeId === 1 && <Nav.Link className='navbar-link' href='/admin'>Admin</Nav.Link>}
+                            </>
+                        )}
+                        
                     </Nav>
                 </NavbarCollapse>
                 <Form className="d-flex">
