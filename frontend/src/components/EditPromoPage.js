e237bce856b9e388d7d640f7c0f2fcb2568bb24a
DSM11 <dantesebmata@gmail.com>
2023-11-05 14:19:57 -0500

    get call working

--- frontend/src/components/EditPromoPage.js
+++ frontend/src/components/EditPromoPage.js
@@ -1,12 +1,51 @@
 import React, { useState, useEffect } from "react";
 //import {useNavigate} from 'react-router-dom';
 import "bootstrap/dist/css/bootstrap.min.css";
+import "../css/EditPromoPage.css";
 import { useNavigate } from "react-router-dom";
+import { Container } from "react-bootstrap";
 
 function EditPromoPage(props) {
+    const [promotionCode, setPromoCode] = useState("");
+    const [promotionPercent, setPromotionPercent] = useState(0);
 
+    const populateInputs=async()=>{
+        try {
+            const response = await fetch(
+              "http://localhost:8080/api/v1/promotion/getByPromoCode/"+props.promoCode
+            );
+            const json = await response.json();
+      
+            if (json) {
+                const {
+                promoCode,
+                discount,
+                startDate,
+                endDate,
+                promoId,
+                promoSent,
+                } = json;
+
+                setPromoCode(promoCode);
+                setPromotionPercent(discount);
+                setStartDate(startDate);
+            }
+            } catch (error) {
+                console.log(error);
+            }
+    };
+    useEffect(() => {
+        populateInputs();
+      }, []);
+    
     return (
-        <div>{props.promoCode}</div>
+        <div id="holder">
+            hello
+            <Container id="editPromoContainer">
+                promoCode:{promotionCode}
+                promotionPercent:{promotionPercent}
+            </Container>
+        </div>
     );
 }
 export default EditPromoPage;
