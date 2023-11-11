24885ef81afd81a719fc88093c0a6c6eb6ed4551
Amit-Kondreddy <ak14884@uga.edu>
2023-11-11 09:28:28 -0500

    completed booking backend

---
+++ backend/src/main/java/com/ecinema/backend/controller/BookingController.java
@@ -0,0 +1,34 @@
+package com.ecinema.backend.controller;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ecinema.backend.models.Booking;
+import com.ecinema.backend.input.BookingInput;
+import com.ecinema.backend.service.BookingService;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+
+@RestController
+@RequestMapping("/api/v1/movie")
+@CrossOrigin("http://localhost:3000")
+public class BookingController {
+
+    @Autowired
+    @Qualifier("bookingService")
+    private BookingService bookingService;
+
+    @PostMapping("/createBooking")
+    public ResponseEntity<Booking> createBooking(@RequestBody BookingInput input) {
+        Booking booking = this.bookingService.createBooking(input);
+        return ResponseEntity.status(HttpStatus.CREATED).body(booking);
+    }
+    
+}
