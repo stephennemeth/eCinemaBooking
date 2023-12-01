0298290301285b579d1bc96fad85687de717e4cb
Stephen Nemeth <83983441+stephennemeth@users.noreply.github.com>
2023-12-01 09:31:18 -0500

    Added Order History

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -58,6 +58,8 @@ const NavBar = (props) => {
                                 <Nav.Link className='navbar-link' href="/updateprofile">Edit Profile</Nav.Link>
                                 <Button className='navbar-link' onClick={logout}>Logout</Button>
                                 {props.user.userTypeId === 1 && <Nav.Link className='navbar-link' href="/admin">Admin</Nav.Link>}
+                                <Nav.Link className='navbar-link' href='/purchasehistory'>Order History</Nav.Link>
+
                             </>
                         )}
                         
